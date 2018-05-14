78060b0d4597b2b80feb281475b160701f5092d5
fiksi <dyahrengganis88@gmail.com>
2018-05-15 00:45:35 +0700

    ini awal baru

---
+++ tes.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define tunda 200
+
+int main(void){
+	DDRG = 0xff;
+	PORTG = 0xff;
+	
+	while(1){
+		PORTG ^= (1<<0);
+		_delay_ms(tunda);
+	}
+	
+}
