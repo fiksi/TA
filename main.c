368f912066f7674e2420674f1c6c74def0ef6126
fiksi <dyahrengganis88@gmail.com>
2018-05-15 02:39:21 +0700

    example MMC udah jalan

---
+++ main.c
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------*/
+/* Foolproof FatFs sample project for AVR              (C)ChaN, 2014    */
+/*----------------------------------------------------------------------*/
+
+#include <avr/io.h>	/* Device specific declarations */
+#include <util/delay.h>
+#include "ff.h"		/* Declarations of FatFs API */
+
+FATFS FatFs;		/* FatFs work area needed for each volume */
+FIL Fil;			/* File object needed for each open file */
+
+
+int main (void)
+{
+	UINT bw;
+	
+	DDRG = 0xFF;
+	PORTG = 0xFF;
+
+	f_mount(&FatFs, "", 0);		/* Give a work area to the default drive */
+
+	if (f_open(&Fil, "newfile.txt", FA_WRITE | FA_CREATE_ALWAYS) == FR_OK) {	/* Create a file */
+
+		f_write(&Fil, "It works!\r\n", 11, &bw);	/* Write data to the file */
+
+		f_close(&Fil);								/* Close the file */
+	}
+
+	while(1){
+		PORTG ^= (1 << 0);
+		_delay_ms(200);
+	}
+}
+
+
